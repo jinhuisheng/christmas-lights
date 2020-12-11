fb5bab84203ea4f925d9b12161089c51804d5cc2
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 21:14:49 +0800

    empty message

---
+++ src/test/java/LightGridTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author huisheng.jin
+ * @date 2020/12/11.
+ */
+public class LightGridTest {
+    @Test
+    void should_open_all_light_success() {
+        LightGrid lightGrid = new LightGrid();
+    }
+}
