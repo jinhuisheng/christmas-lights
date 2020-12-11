b69c9856f46e36b6a7eab5185c917669b4521d33
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 21:37:59 +0800

    empty message

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -3,7 +3,16 @@
  * @date 2020/12/11.
  */
 public class Coordinate {
+    private final int x;
+    private final int y;
+
     public Coordinate(int x, int y) {
 
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
     }
 }
