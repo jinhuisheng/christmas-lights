eb3bab4cc2ea866c018e144356e7576eea0a5eff
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-11 21:35:36 +0800

    empty message

--- src/main/java/LightGrid.java
+++ src/main/java/LightGrid.java
@@ -8,7 +8,7 @@ public class LightGrid {
     private Light[][] lights;
 
     public LightGrid(int x, int y) {
-        lights = new Light[1000][1000];
+        lights = new Light[x][y];
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 1000; j++) {
                 lights[i][j] = new Light();
